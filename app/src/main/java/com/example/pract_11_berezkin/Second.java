ce7ab8cbb6f2279bad5ca6c31a177c5ca3116f86
Aleksandr <aleksberezkin68@mail.ru>
2021-12-03 21:15:05 +0900

    pract_11
    pract_11berezkin

---
+++ app/src/main/java/com/example/pract_11_berezkin/Second.java
@@ -0,0 +1,12 @@
+package com.example.pract_11_berezkin;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Second extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+    }
+}
